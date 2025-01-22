ae5dd44575b1acb4afbdb60b8f90d004232c1605
Divyesh <143060448+Divyesh-Thakur45@users.noreply.github.com>
2025-01-22 16:04:34 +0530

    Update isAuth.js

--- server/middlewares/isAuth.js
+++ server/middlewares/isAuth.js
@@ -1,23 +1,18 @@
 import jwt from "jsonwebtoken";
 
 const isAuth = (req, res, next) => {
-  console.log(req.cookies);
   const { token } = req.cookies;
-    if (!token) {
-      return res.status(401).send("Unauthorized");
-    } else {
-      // verify a token symmetric
-      jwt.verify(token, "zxcvbnm", (err, decoded) => {
-        if (err) {
-          return res.status(403).send("Token is not valid");
-        } else {
-          // console.log(req);
-        //   req.user = decoded.UserData;
-          console.log("auth successful");
-          next();
-        }
-      });
+  if (!token) {
+    return res.status(401).send("Unauthorized: No token provided");
+  }
+  jwt.verify(token, "zxcvbnm", (err, decoded) => {
+    if (err) {
+      return res.status(403).send("Token is invalid");
     }
+    req.user = decoded.UserData; // Pass user data to the next middleware
+    console.log("Authorization successful");
+    next();
+  });
 };
 
 export { isAuth };
