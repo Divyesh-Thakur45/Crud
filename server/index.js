03c6dabf79cb9ca63ff36df1a0c85873db087a3a
Divyesh <143060448+Divyesh-Thakur45@users.noreply.github.com>
2025-01-22 16:58:01 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://crud-g2k4.onrender.com",
     credentials: true, // If you are using cookies
   })
 );
