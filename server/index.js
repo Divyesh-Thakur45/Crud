b36afd56fd321991267f309bf332177d05054f93
Divyesh <143060448+Divyesh-Thakur45@users.noreply.github.com>
2025-01-22 17:07:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ['http://localhost:3000',  'https://frontside-eight.vercel.app',],
     credentials: true, // If you are using cookies
   })
 );
